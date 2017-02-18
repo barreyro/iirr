c140aa3f5f9a307d21259e61a70f7fe90618e28d
Stephanie <stephaniebarreyro@gmail.com>
2017-02-18 15:39:34 -0500

    Add ES6 property initializers

--- src/App.js
+++ src/App.js
@@ -5,22 +5,15 @@ import {TodoForm, TodoList} from './components/todo';
 import {addTodo, generateId} from './lib/todoHelpers';
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      todos: [
-        {id: 1, name: 'Learn JSX', isComplete: true},
-        {id: 2, name: 'Build an awesome app', isComplete: false},
-        {id: 3, name: 'Ship it!', isComplete: false}
-      ],
-      currentTodo: ''
-    }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleEmptySubmit = this.handleEmptySubmit.bind(this)
+  state = {todos: [
+      { id: 1, name: 'Learn JSX', isComplete: true },
+      { id: 2, name: 'Build an awesome app', isComplete: false },
+      { id: 3, name: 'Ship it!', isComplete: false }
+    ],
+    currentTodo: ''
   }
   
-  handleSubmit(evt) {
+  handleSubmit = (evt) => {
     evt.preventDefault()
     const newId = generateId()
     const newTodo = {id: newId, name: this.state.currentTodo, isComplete: false}
@@ -29,17 +22,17 @@ class App extends Component {
       todos: updatedTodos,
       currentTodo: '',
       errorMessage: ''
-    })
+    });
   }
 
-  handleEmptySubmit(evt) {
+  handleEmptySubmit = (evt)  => {
     evt.preventDefault()
     this.setState({
       errorMessage: 'Please supply a todo name'
     })
   }
 
-  handleInputChange(evt) {
+  handleInputChange = (evt) =>  {
     this.setState({
       currentTodo: evt.target.value
     });
