20c8b5a6ab8147d50dc9373a4d2c508afdb47671
Stephanie <stephaniebarreyro@gmail.com>
2017-02-20 14:53:40 -0500

    Filter data on property values

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {TodoForm, TodoList, Footer} from './components/todo';
-import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo} from './lib/todoHelpers';
+import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './lib/todoHelpers';
 import {pipe, partial} from './lib/utils'
 
 class App extends Component {
@@ -12,6 +12,10 @@ class App extends Component {
       { id: 3, name: 'Ship it!', isComplete: false }
     ],
     currentTodo: ''
+  }
+
+  static contextTypes ={
+    route: React.PropTypes.string
   }
 
   handleRemove = (id, evt) => {
@@ -53,6 +57,7 @@ class App extends Component {
 
   render() {
     const submitHandler = this.state.currentTodo.trim() ? this.handleSubmit : this.handleEmptySubmit
+    const displayTodos = filterTodos(this.state.todos, this.context.route)
     return (
       <div className="App">
         <div className="App-header">
@@ -64,7 +69,8 @@ class App extends Component {
           <TodoForm handleInputChange={this.handleInputChange}
             currentTodo={this.state.currentTodo}
             handleSubmit={submitHandler}/>
-          <TodoList handleToggle={this.handleToggle} todos={this.state.todos} 
+          <TodoList handleToggle={this.handleToggle} 
+          todos={displayTodos} 
           handleRemove={this.handleRemove}/>
           <Footer />
         </div>
