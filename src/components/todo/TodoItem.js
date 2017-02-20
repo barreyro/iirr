6a07e141e5263eb28aee0849e6a219308c40db5c
Stephanie <stephaniebarreyro@gmail.com>
2017-02-20 14:02:17 -0500

    Remove items from the list

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -3,9 +3,13 @@ import {partial} from '../../lib/utils'
 
 export const TodoItem = (props) => {
   const handleToggle = partial(props.handleToggle, props.id)
+  const handleRemove = partial(props.handleRemove, props.id)
 
   return (
     <li>
+      <span className='delete-item'>
+        <a href="#" onClick={handleRemove}>x</a>
+      </span>
       <input type="checkbox" onChange={ () => props.handleToggle(props.id)} 
       checked={props.isComplete}/> {props.name}
     </li>
