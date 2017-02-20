595fad7b84b2ca03fe640675a8b451c51e4e3da1
Stephanie <stephaniebarreyro@gmail.com>
2017-02-20 13:39:51 -0500

    Create a pipe function in utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,1 +1,7 @@
 export const partial = (fn, ...args) => fn.bind(null, ...args)
+
+//pipe takes 2 functions, f and g
+//returns another function
+const _pipe = (f, g) => (...args) => g(f(...args))
+
+export const pipe = (...fns) => fns.reduce(_pipe)
